0ed59d8be61e269cb15e572f81c4496b07479bfc
BAEKJungHo <designjava@naver.com>
2023-05-28 13:13:30 +0900

    Create Projects

---
+++ coroutinepractice/src/main/kotlin/com/coroutine/demo/CoroutinepracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.coroutine.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutinepracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoroutinepracticeApplication>(*args)
+}
---
+++ coroutinepractice/src/test/kotlin/com/coroutine/demo/CoroutinepracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coroutine.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutinepracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
